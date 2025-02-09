f07fb00fee9dfba50cfa8ad9411e7cdeaa50f863
Othan2 <owenboyd2@gmail.com>
2025-02-08 16:44:12 -0800

    Clean up client.go

--- client.go
+++ client.go
@@ -10,19 +10,8 @@ import (
 	"github.com/bluesky-social/indigo/api/bsky"
 )
 
-// Client is the interface that provides methods to interact with a Bluesky PDS instance.
-// TODO: split into sub-services to better group operations and reduce clutter.
-// Example grouping: profile, posts, timeline
-
-// TODO: maybe delete? seems ok to export the concrete type.
+// Client to interact with AT Protocol PDSs.
 type Client interface {
-	// TODO: move this documentation to NewClient. Probably also want to move client implementation to its own file.
-	// Login authenticates to the Bluesky server with the given handle and appkey.
-	// Note: authenticating with a live password instead of an application key will
-	// be detected and rejected. For your security, this library will refuse to use
-	// your master credentials.
-	// Login(ctx context.Context, handle string, appkey string) error
-
 	// Close terminates the client, shutting down all pending tasks and background operations.
 	Close() error
 
